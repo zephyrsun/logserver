23ce3f495c3be1ea97c7fb42985a5442e896e029
zephyrsun <zhenghua.sun@gmail.com>
2014-07-10 14:56:07 +0800

    client.go: add parameter: -addr

--- example/client.go
+++ example/client.go
@@ -8,12 +8,13 @@ import (
 
 func main() {
 
+	addr := flag.String("addr", ":8282", "Server address")
 	n := flag.Int("n", 100000, "Number of requests to perform")
 	d := flag.String("d", "1=2014-07-10 13:57:40|200|1|2|3|4|5|6|||||||||1111111111|2222222222|3333333333|from|tttttttt||||||||||||||||&2=2014-07-10 14:14:58|2014-07-10 13:57:40|200|1|2|3|4|5|6|||||||||1111111111|2222222222|3333333333|from|tttttttt||||||||||||||||", "Data to be sent")
 
 	flag.Parse()
 
-	conn, err := net.Dial("udp", ":8282")
+	conn, err := net.Dial("udp", *addr)
 	logserver.PanicOnError(err)
 
 	b := []byte(*d)
