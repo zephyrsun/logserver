f815f4669e883467cffc5c476e076885a1e1a5e6
zephyrsun <zhenghua.sun@gmail.com>
2014-07-11 11:11:59 +0800

    improve bench_test.go

--- test/bench_test.go
+++ test/bench_test.go
@@ -31,9 +31,26 @@ func BenchmarkWrite(b *testing.B) {
 	str2 := "2014-07-10 10:47:08|700|1|||||1|||||||||1111111111|2222222222|3333333333|from|tttttttt||||||||||||||||"
 	strByte2 := []byte(str2)
 
+	c1 := make(chan int)
+	c2 := make(chan int)
+
+	go func() {
+		for {
+			<-c1
+			srv.Write(k1, strByte1)
+		}
+	}()
+
+	go func() {
+		for {
+			<-c2
+			srv.Write(k2, strByte2)
+		}
+	}()
+
 	for i := 0; i < b.N; i++ { //use b.N for looping
-		srv.Write(k1, strByte1)
-		srv.Write(k2, strByte2)
+		c1<-i
+		c2<-i
 	}
 }
 
